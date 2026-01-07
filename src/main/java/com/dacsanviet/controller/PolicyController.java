aedc17e20b3c9792df8dd77834717d3433c8eba1
teehihi <tee21052005@gmail.com>
2026-01-07 13:16:00 +0700

    Fix some bug

--- src/main/java/com/dacsanviet/controller/PolicyController.java
+++ src/main/java/com/dacsanviet/controller/PolicyController.java
@@ -9,10 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
  */
 @Controller
 public class PolicyController {
-    
-    @GetMapping("/shipping-policy")
-    public String shippingPolicy(Model model) {
-        model.addAttribute("pageTitle", "Chính Sách Vận Chuyển");
-        return "shipping-policy";
-    }
+
+	@GetMapping("/shipping-policy")
+	public String shippingPolicy(Model model) {
+		model.addAttribute("pageTitle", "Chính Sách Vận Chuyển");
+		return "shipping-policy";
+	}
+
+	@GetMapping("/privacy-policy")
+	public String privacyPolicy(Model model) {
+		model.addAttribute("pageTitle", "Chính Sách Bảo Mật");
+		return "privacy-policy";
+	}
+
+	@GetMapping("/terms-of-service")
+	public String termsOfService(Model model) {
+		model.addAttribute("pageTitle", "Điều Khoản Sử Dụng");
+		return "terms-of-service";
+	}
 }
